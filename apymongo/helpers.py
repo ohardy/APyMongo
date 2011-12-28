5985ce2ec0e9bccc167e23f7981bba66bd281a7a
Olivier Hardy <ohardy@me.com>
2011-12-28 15:34:35 +0100

    Import consts from consts

--- apymongo/helpers.py
+++ apymongo/helpers.py
@@ -24,7 +24,8 @@ import struct
 
 import bson
 from bson.son import SON
-import pymongo
+
+from consts import ASCENDING, DESCENDING, GEO2D
 import apymongo
 from apymongo.errors import (AutoReconnect,
                             OperationFailure,
@@ -33,14 +34,14 @@ from apymongo.errors import (AutoReconnect,
 
 def _index_list(key_or_list, direction=None):
     """Helper to generate a list of (key, direction) pairs.
-
+    
     Takes such a list, or a single key, or a single key and direction.
     """
     if direction is not None:
         return [(key_or_list, direction)]
     else:
         if isinstance(key_or_list, basestring):
-            return [(key_or_list, pymongo.ASCENDING)]
+            return [(key_or_list, ASCENDING)]
         elif not isinstance(key_or_list, list):
             raise TypeError("if no direction is specified, "
                             "key_or_list must be an instance of list")
@@ -49,7 +50,7 @@ def _index_list(key_or_list, direction=None):
 
 def _index_document(index_list):
     """Helper to generate an index specifying document.
-
+    
     Takes a list of (key, direction) pairs.
     """
     if isinstance(index_list, dict):
@@ -61,12 +62,12 @@ def _index_document(index_list):
                         "not: " + repr(index_list))
     if not len(index_list):
         raise ValueError("key_or_list must not be the empty list")
-
+    
     index = SON()
     for (key, value) in index_list:
         if not isinstance(key, basestring):
             raise TypeError("first item in each key pair must be a string")
-        if value not in [pymongo.ASCENDING, pymongo.DESCENDING, pymongo.GEO2D]:
+        if value not in [ASCENDING, DESCENDING, GEO2D]:
             raise TypeError("second item in each key pair must be ASCENDING, "
                             "DESCENDING, or GEO2D")
         index[key] = value
@@ -75,10 +76,10 @@ def _index_document(index_list):
 
 def _unpack_response(response, cursor_id=None, as_class=dict, tz_aware=False):
     """Unpack a response from the database.
-
+    
     Check the response for errors and unpack, returning a dictionary
     containing the response data.
-
+    
     :Parameters:
       - `response`: byte string as returned from the database
       - `cursor_id` (optional): cursor_id we sent to get this response -
@@ -90,7 +91,7 @@ def _unpack_response(response, cursor_id=None, as_class=dict, tz_aware=False):
     if response_flag & 1:
         # Shouldn't get this response if we aren't doing a getMore
         assert cursor_id is not None
-
+        
         raise OperationFailure("cursor id '%s' not valid at server" %
                                cursor_id)
     elif response_flag & 2:
@@ -99,7 +100,7 @@ def _unpack_response(response, cursor_id=None, as_class=dict, tz_aware=False):
             raise AutoReconnect("master has changed")
         raise OperationFailure("database error: %s" %
                                error_object["$err"])
-
+    
     result = {}
     result["cursor_id"] = struct.unpack("<q", response[4:12])[0]
     result["starting_from"] = struct.unpack("<i", response[12:16])[0]
@@ -118,7 +119,7 @@ def _check_command_response(response, reset, msg="%s", allowable_errors=[]):
                 reset()
                 return AutoReconnect("not master")
             return OperationFailure(msg % response["errmsg"])
-
+    
     return response
 
 def _password_digest(username, password):
@@ -128,7 +129,7 @@ def _password_digest(username, password):
         raise TypeError("password must be an instance of basestring")
     if not isinstance(username, basestring):
         raise TypeError("username must be an instance of basestring")
-
+    
     md5hash = _md5func()
     md5hash.update("%s:mongo:%s" % (username.encode('utf-8'),
                                     password.encode('utf-8')))
@@ -146,11 +147,11 @@ def _auth_key(nonce, username, password):
 
 def _fields_list_to_dict(fields):
     """Takes a list of field names and returns a matching dictionary.
-
+    
     ["a", "b"] becomes {"a": 1, "b": 1}
-
+    
     and
-
+    
     ["a.b.c", "d", "a.c"] becomes {"a.b.c": 1, "d": 1, "a.c": 1}
     """
     as_dict = {}
